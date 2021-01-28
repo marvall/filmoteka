5af9e2688f94ba617ac1ae2d3385af3e6282d725
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-01-28 13:16:40 +0200

    add render check

--- src/js/components/nav/checkNavigation.js
+++ src/js/components/nav/checkNavigation.js
@@ -2,14 +2,35 @@ import { changeHistory } from '../../utils/changeHistory';
 import { renderGallery } from '../renderGallery';
 import { getFilms } from '../api';
 
+const header = document.querySelector('[data-index="header"]');
+const homeBtn = document.querySelector('[data-index="home"]');
+const mylibraryBtn = document.querySelector('[data-index="mylibrary"]');
+const gallery = document.querySelector('[data-index="gallery"]');
+console.log(header);
+
 export const checkNavigation = function (e) {
   e.preventDefault();
+
   if (e.target !== e.currentTarget) {
-    if (e.target.textContent == 'FILMOTEKA' || e.target.textContent == 'HOME') {
+    if (
+      e.target.parentElement.nodeName === 'A' ||
+      e.target.nodeName === 'A' ||
+      e.target.nodeName === 'use' ||
+      e.target.textContent === 'HOME'
+    ) {
       changeHistory('home');
       getFilms().then(data => renderGallery(data));
+      header.classList.remove('header__background-myLibrary');
+      header.classList.add('header__background-home');
+      mylibraryBtn.classList.remove('current');
+      homeBtn.classList.add('current');
     } else if (e.target.textContent == 'MY LIBRARY') {
       changeHistory('mylibrary');
+      header.classList.remove('header__background-home');
+      header.classList.add('header__background-myLibrary');
+      gallery.innerHTML = '';
+      homeBtn.classList.remove('current');
+      mylibraryBtn.classList.add('current');
     }
   }
 };
