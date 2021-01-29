98c08fe41eb8d34b5af5559ddea1f510625d01b8
Olena Tsemko <elena.hlihan@gmail.com>
2021-01-29 19:08:25 +0200

    pagination init

---
+++ src/js/components/pagination/paginationInit.js
@@ -0,0 +1,67 @@
+import * as $ from 'jquery';
+
+import './paginationPlugin';
+import { renderGallery } from '../renderGallery';
+import { getFilmsPagination } from '../api';
+
+async function initPagination(query) {
+  const container = $('[data-index="pagination"]');
+
+  const data = await getFilmsPagination(query);
+  const sources = data.results;
+  const totalResults = data.total_results;
+
+  const arrayCountElem = new Array(totalResults - data.results.length);
+  const dataArray = sources.concat(arrayCountElem);
+
+  const options = {
+    dataSource: dataArray,
+
+    pageSize: 20,
+    pageRange: 2,
+
+    autoHidePrevious: true,
+    autoHideNext: true,
+
+    triggerPagingOnInit: false,
+
+    callback: function (response, pagination) {
+      var dataHtml = renderGallery(response);
+      dataHtml += '</ul>';
+
+      container.prev().html(dataHtml);
+    },
+  };
+
+  container.pagination(options);
+
+  const paginationWrapper = document.querySelector('[data-index="pagination"]');
+  paginationWrapper.addEventListener('mouseup', onClickPageHandler);
+
+  function onClickPageHandler(event) {
+    if (event.target.parentNode.classList.contains('paginationjs-ellipsis')) {
+      console.log(event.target.parentNode.dataset.num);
+
+      return;
+    }
+
+    let num = 0;
+
+    if (event.target.nodeName === 'A') {
+      num = +event.target.parentNode.dataset.num;
+    } else if (event.target.nodeName === 'LI') {
+      num = +event.target.dataset.num;
+    } else {
+      return;
+    }
+
+    getFilmsPagination(query, num).then(({ results }) => {
+      renderGallery(results);
+    });
+  }
+}
+
+export default initPagination;
+
+initPagination();
+// initPagination(null);
