13f74ccc895ce9059ebfb7abf7b889d41da0c6a4
Ihor Prokopets <kiringa23@gmail.com>
2021-01-30 11:38:28 +0200

    add basic logic for search on input,add debounce for search

---
+++ src/js/components/keyWorldSearch.js
@@ -0,0 +1,20 @@
+//  Пока закоментил, так как при динамическом рендере выдаёт ошибку
+
+// import { getFilms } from './api';
+
+// import { renderGallery } from './renderGallery';
+
+// const _ = require('lodash');
+
+// const refs = {
+//   searchForm: document.querySelector('[data-index="serchInfo"]'),
+//   fetchStatus: document.querySelector('[data-index="fetchStatus"]'),
+// };
+
+// refs.searchForm.addEventListener(
+//   'input',
+//   _.debounce(e => {
+//     e.preventDefault();
+//     getFilms(e.target.value).then(data => renderGallery(data));
+//   }, 1250),
+// );
