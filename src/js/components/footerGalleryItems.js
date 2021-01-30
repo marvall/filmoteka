f3fd8915867e810a7667398bb512bbe0573674bb
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-01-30 13:01:53 +0200

    add images src

--- src/js/components/footerGalleryItems.js
+++ src/js/components/footerGalleryItems.js
@@ -1,52 +1,50 @@
 export default [
-    {
-        name: 'Yaroslav',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Anton',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Alex',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Artem',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Andrii',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Ihor',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Lena',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },
-    {
-        name: 'Viki',
-        photo: '../../images/background-mobile.jpg',
-        instagram: 'https://www.instagram.com/mira.nikolaenko',
-        facebook: 'https://www.facebook.com/mira.abdulaeva/',
-    },    
+  {
+    name: 'Yaroslav',
+    photo: '../../images/team-photo/Yaroslav.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Anton',
+    photo: '../../images/team-photo/Anton.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Alex',
+    photo: '../../images/background-mobile.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Artem',
+    photo: '../../images/background-mobile.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Andrii',
+    photo: '../../images/background-mobile.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Ihor',
+    photo: '../../images/background-mobile.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Lena',
+    photo: '../../images/background-mobile.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
+  {
+    name: 'Viki',
+    photo: '../../images/background-mobile.jpg',
+    instagram: 'https://www.instagram.com/mira.nikolaenko',
+    facebook: 'https://www.facebook.com/mira.abdulaeva/',
+  },
 ];
-
-
