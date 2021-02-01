51fa3ac7f7427a7e8e1480abda30d99be499a576
marvall <yariknarik.xxx@gmail.com>
2021-02-01 16:58:38 +0200

    add some logic + style

--- src/js/components/nav/checkNavigation.js
+++ src/js/components/nav/checkNavigation.js
@@ -15,8 +15,11 @@ import initPaginationLS from '../pagination/paginationLS';
 import { addToStorage } from '../addToStorage';
 import getFromStorage from '../getFromStorage';
 
-const changeStartedPage = function (address) {
-  changeHistory(address);
+/**
+ * This function render started-page: home.
+ */
+const changeStartedPage = function () {
+  changeHistory('home');
   homePageMarkupUpdate();
   spinner('start');
   getFilmsPagination().then(data => {
@@ -27,58 +30,97 @@ const changeStartedPage = function (address) {
   spinner('stop');
 };
 
+const chechFilmInStack = function (e) {
+  //console.log(e.target.dataset.index);
+  //if (
+  //  e.target.dataset.index === 'btn-to-wached' ||
+  //  e.target.dataset.index === 'btn-to-queue'
+  //) {
+  //  let stackName = e.target.dataset.index;
+  //  console.log(stackName);
+  //  if (stackName === 'btn-to-wached') {
+  //    stackName = 'watched';
+  //    console.log(stackName);
+  //  }
+  //  if (stackName === 'btn-to-queue') {
+  //    stackName = 'queue';
+  //    console.log(stackName);
+  //  }
+  //
+  //  let stack = getFromStorage(stackName);
+  //
+  //  const stackId = stack.map(film => film.id);
+  //  let cardID = document.querySelector("[data-index='cardInfo']");
+  //
+  //  let BtnWatched = document.querySelector("[data-index='btn-to-wached']");
+  //  let BtnQueue = document.querySelector("[data-index='btn-to-queue']");
+  //
+  //  if (stackId.includes(Number(cardID.id))) {
+  //    e.target.classList.add('activeBtn');
+  //    e.target.textContent = 'Remove';
+  //  } else {
+  //    e.target.classList.remove('activeBtn');
+  //    e.target.textContent = `ADD TO ${stackName.toUpperCase()}`;
+  //  }
+  //}
+};
+
+/**
+ * This Function expects events. The Func handles clicks on all interfaces,
+ * navigates or calls the desired stack of actions for a specific element.
+ * @param {event} e
+ */
 export const checkNavigation = function (e) {
   e.preventDefault();
   if (e.type === 'DOMContentLoaded') {
     //Started HOME PAGE
-    changeStartedPage('home');
+    changeStartedPage();
   } else if (e.target !== e.currentTarget) {
     if (checkClickTarget(e)) {
       //Started HOME PAGE
-      changeStartedPage('home');
+      changeStartedPage();
     } else if (e.target.textContent === 'MY LIBRARY') {
       document.querySelector("[data-index='pagination']").innerHTML = '';
+      //Started MYLIBRARY PAGE
       changeHistory('mylibrary');
       myLibraryPageMarkupUpdate();
       headerDinamicContentMarkupUpdate();
+      document
+        .querySelector('[data-index="watched"]')
+        .classList.add('current__myLibraryBtn');
       spinner('start');
-
-      // ==============================================================
-      //тут должна быть отрисовка моей библиотеки.
-      // массив внутри для проверки, его удалить, передать параметром массив с локал сторейджа, этот же массив импортировать в файл paginationLS.js, и записать в переменную results
+      //RENDER STACK WATCHED
       initPaginationLS(getFromStorage('watched'));
-      // ==============================================================
-
       spinner('stop');
     } else if (e.target.parentNode.dataset.index === 'card') {
-      //this func open modal in gallery
+      //THIS FUNC OPEN MODLA IN GALLERY
       setModalAttribute(e.target.parentNode);
+      chechFilmInStack(e);
     } else if (e.target.dataset.index === 'team') {
-      //this func open modal in footer
+      //THIS FUNC OPEN MODLA IN FOOTER
       showTeam(e.target);
     } else if (e.target.dataset.index === 'btn-to-wached') {
+      //ADD TO WATCHED
+      chechFilmInStack(e);
       getFilmInfo(document.querySelector('[data-index="cardInfo"]').id).then(
         data => {
           addToStorage(data, 'watched');
         },
       );
     } else if (e.target.dataset.index === 'btn-to-queue') {
+      //ADD TO QUEUE
+      chechFilmInStack(e);
       getFilmInfo(document.querySelector('[data-index="cardInfo"]').id).then(
         data => {
           addToStorage(data, 'queue');
         },
       );
     } else if (e.target.dataset.index === 'watched') {
-      // let slon = getFromStorage('watched');
-      // console.log(slon.length);
+      //RENDER STACK WATCHED
       initPaginationLS(getFromStorage('watched'));
-      //initPagination({
-      //  results: slon,
-      //  total_pages: slon.length / 20 + 1,
-      //});
     } else if (e.target.dataset.index === 'queue') {
+      //RENDER STACK QUEUE
       initPaginationLS(getFromStorage('queue'));
-      //initPagination(getFromStorage('queue'));
     }
   }
 };
