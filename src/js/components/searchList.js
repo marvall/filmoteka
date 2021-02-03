11a81e299dcf6f0ce55e9a05d4b3b1c1d982e091
Atrem Sergieiev <a_serg83@ukr.net>
2021-02-04 00:55:33 +0100

    fix search list

--- src/js/components/searchList.js
+++ src/js/components/searchList.js
@@ -1,17 +1,43 @@
 import searchListTempl from '../../templates/searchListTempl.hbs';
 
-function renderListSearch(objects) {
-  let arrRender = sortByRating(objects);
+function renderListSearch(searchValue, objects) {
+  if (!searchValue) {
+    deleteListSearch();
+    return;
+  }
+  let cloneObjects = JSON.parse(JSON.stringify(objects));
+  let arrRender = sortByRating(cloneObjects);
   if (arrRender.length > 6) {
-    arrRender = objects.slice(0, 5);
+    arrRender = cloneObjects.slice(0, 5);
   }
   const searchListRef = document.querySelector("[data-index='card-list']");
+  searchListRef.classList.add('open');
   const markup = searchListTempl(arrRender);
   searchListRef.innerHTML = markup;
+
+  // searchListRef.addEventListener('click', handlerOpenFilm);
 }
 
 function sortByRating(array) {
   return array.sort((a, b) => b.vote_average - a.vote_average);
 }
 
-export { renderListSearch };
+function deleteListSearch() {
+  const searchListRef = document.querySelector("[data-index='card-list']");
+  searchListRef.innerHTML = '';
+  searchListRef.classList.remove('open');
+  // searchListRef.removeEventListener('click', handlerOpenFilm);
+}
+
+// function handlerOpenFilm(e) {
+//   e.preventDefault();
+//   console.log('click list item', e.target);
+//   // deleteListSearch();
+//   if (!e.target.parentNode) {
+//     console.log('close');
+//     // deleteListSearch();
+//     // return;
+//   }
+// }
+
+export { renderListSearch, deleteListSearch };
