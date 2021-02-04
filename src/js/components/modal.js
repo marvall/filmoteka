02ea46ab223ce653c1a3ee5d3ef47f50f6a80db5
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2021-02-04 18:40:18 +0200

    add modal-video

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -5,6 +5,7 @@ import footerGalleryItem from '../../js/components/footerGalleryItems';
 import { getFilmInfo } from './api';
 import { spinner } from './spinner';
 import { checkFilmInStack } from './checkFimlInStack';
+import { getVideoUrl } from './getVideoUrl';
 
 MicroModal.init();
 // * This function removes markup of the previous modal window,
@@ -47,5 +48,37 @@ export function showTeam(Node) {
     .insertAdjacentHTML('beforeend', modalFooter(footerGalleryItem));
   MicroModal.show('modal-1');
     Node.removeAttribute('data-micromodal-trigger')
+  
+}
+
+/**
+ * You need to pass the node(button Youtube on the card of one film) to this function.
+ * @param {Node} event
+ */
+export function showVideo(Node) {
+  Node.setAttribute('data-micromodal-trigger', 'modal-1');
+  getVideoUrl(Node.id).then(url => {
+    const markup = `<div class="modal__overlay" tabindex="-1"
+                   data-micromodal-close>
+                  <div class="modal__container" role="dialog" 
+                  aria-modal="true" aria-labelledby="modal-1-title">
+                    <main class="modal__content">
+                      <div class="movie-tmpl" data-index="modal-video">
+                        <iframe width="560" height="315" src=${url} 
+                        frameborder="0"
+                         allow="accelerometer; autoplay; clipboard-write;
+                          encrypted-media; gyroscope; picture-in-picture"
+                         allowfullscreen>
+                        </iframe>
+                      </div>
+                    </main>
+                  </div>
+                </div >`;
+    document.querySelector('#modal-1').innerHTML = '';
+    document.querySelector('#modal-1').insertAdjacentHTML('beforeend', markup);
+    });
+    MicroModal.show('modal-1');
+    Node.removeAttribute('data-micromodal-trigger');
+  
   
 }
