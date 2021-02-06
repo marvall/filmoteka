654a1e4a4fe435e95b8efc67b68eecc31e544c40
marvall <yariknarik.xxx@gmail.com>
2021-02-06 09:32:39 +0200

    save

--- src/js/components/firebase/firebase-auth.js
+++ src/js/components/firebase/firebase-auth.js
@@ -71,22 +71,44 @@ function app(user) {
 }
 
 export function obFromIndexedDB() {
-  const dump = {};
-  const dbRequest = window.indexedDB.open('firebaseLocalStorageDb');
-  dbRequest.onsuccess = () => {
-    const localdb = dbRequest.result;
-    const stores = ['firebaseLocalStorage'];
-    const tx = localdb.transaction(stores);
-    const req = tx.objectStore(stores).getAll();
-    req.onsuccess = () => {
-      dump[stores] = req.result;
-      dump[stores].forEach(elem => {
-        app(elem.value);
-        State.Auth = elem.value.uid;
-        getFromDB(State.Auth);
-      });
+  //fix was finded on :
+  //https://coderoad.ru/17468963/%D0%9F%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D1%8C%D1%82%D0%B5-%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D1%83%D0%B5%D1%82-%D0%BB%D0%B8-%D0%B1%D0%B0%D0%B7%D0%B0-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-IndexedDB
+  function databaseExists(dbname, callback) {
+    let req = indexedDB.open(dbname);
+    let existed = true;
+    req.onsuccess = function () {
+      req.result.close();
+      if (!existed) indexedDB.deleteDatabase(dbname);
+      callback(existed);
     };
-  };
+    req.onupgradeneeded = function () {
+      existed = false;
+    };
+  }
+  databaseExists('firebaseLocalStorageDb', function (yesno) {
+    if (yesno) {
+      const dump = {};
+      const dbRequest = window.indexedDB.open('firebaseLocalStorageDb');
+      dbRequest.onsuccess = () => {
+        const localdb = dbRequest.result;
+        if (localdb.objectStoreNames.length === 0) {
+          return;
+        } else {
+          const stores = ['firebaseLocalStorage'];
+          const tx = localdb.transaction(stores);
+          const req = tx.objectStore(stores).getAll();
+          req.onsuccess = () => {
+            dump[stores] = req.result;
+            dump[stores].forEach(elem => {
+              app(elem.value);
+              State.Auth = elem.value.uid;
+              getFromDB(State.Auth);
+            });
+          };
+        }
+      };
+    }
+  });
 }
 
 function renderLoginBtnAfterGetAuthState() {
